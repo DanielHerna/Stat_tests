9252446d9e10d0c53bbbd9b3b3c936a751291265
Daniel Hernandez <82115133+DanielHerna@users.noreply.github.com>
2022-11-10 11:04:00 -0500

    Add files via upload
    scripts with the code for the stat-test

---
+++ scripts/1 sample mean.R
@@ -0,0 +1,52 @@
+rm(list = ls())
+
+#-------------------------------------- Run all the libraries -----------------------------------------------------
+
+library('dplyr')
+library('mosaic')
+
+#-------------------------------------- Only for normal data -----------------------------------------------------
+#-------------------------------------- Parameters to change -----------------------------------------------------
+
+M0<-40          #Hypothesized mean, value to compare
+alpha=0.1       #Level of significance
+
+#------------------------------ run this code, data must be 2x2, ID and the numerical variable --------------------
+
+data<-read.csv(file.choose(),sep = ',')
+sample_size<-nrow(data)
+
+M<-data[,2]
+
+test<-t.test(M, mu = M0, conf.level = 1-alpha)
+cat('with a p value of',round(test$p.value,6),if_else(test$p.value<alpha,'the mean is different from the hypothesized mean','the mean is not different from the hypothesized mean'),'\n')
+
+cat('The margin of error is ',mean(M)-test$conf.int[1],'\n')
+cat('The confidence interval of the proportion is from',round(test$conf.int[1],2), 'to',round(test$conf.int[2],2))
+
+#-------------------------------------- Bootstrap for non-normal data ----------------------------------------------
+#-------------------------------------- Parameters to change -------------------------------------------------------
+rm(list = ls())
+
+alpha=0.1       #Level of significance
+iterations<-10000
+
+#------------------------------ run this code, data must be dim 2x2, ID and the numerical variable --------------------
+
+data<-read.csv(file.choose(),sep = ',')
+
+M<-mean(~data[,2],data=data) 
+bootstrap_dist<-do(iterations)*mean(~data[,2],data=resample(data))
+
+par(mfrow=c(1,2))
+
+qqnorm(bootstrap_dist$mean); qqline(bootstrap_dist$mean, col = 2)
+hist(bootstrap_dist$mean, main='Histogram of the mean')
+
+dev.off()
+
+range<-quantile(sort(bootstrap_dist$mean),c(alpha/2,1-alpha/2))
+cat('The confidence interval for the proportion is from',round(range[1],4),'to',round(range[2],6),'\n')
+cat('The margin of error is ',M-range[1])
+
+
---
+++ scripts/1 sample proportion.R
@@ -0,0 +1,73 @@
+rm(list = ls())
+
+#-------------------------------------- Run all the libraries -----------------------------------------------------
+
+library('dplyr')
+library('mosaic')
+
+#-------------------------------------- Only for normal data -----------------------------------------------------
+#-------------------------------------- Parameters to change -----------------------------------------------------
+
+p0<-0.6          #Hypothesized propotion, value to compare
+alpha=0.1        #Level of significance
+
+#------------------------------ run this code, data must be dim 2x2, ID and the numerical variable --------------------
+
+data<-read.csv(file.choose(),sep = ',')
+sample_size<-nrow(data)
+
+proportion<-table(data[,2])
+proportion<-prop.table(proportion)
+p<-proportion['1']
+
+os_test<-prop.test(x=p, n=sample_size, p=p0, alternative="two.sided", conf.level = 1-alpha)
+cat('with a p value of',round(os_test$p.value,6),if_else(os_test$p.value<alpha,'the proportion is different from the hypothesis','the proportion is not different from the hypothesis'),'\n')
+
+Z_score<-qnorm(p=alpha/2, lower.tail=FALSE)
+ME<-Z_score*sqrt((p*(1-p))/sample_size)
+cat('The margin of error is ',round(ME,4)*100,'%\n')
+cat('The confidence interval of the proportion is from',round(p-ME,4)*100,'%', 'to', round(p+ME,4)*100,'%')
+
+#-------------------------------------- Bootstrap for non-normal data ----------------------------------------------
+#-------------------------------------- Parameters to change -----------------------------------------------------
+rm(list = ls())
+
+alpha<-0.1
+iterations<-10000
+
+#------------------------------ run this code, data must be dim 2x2, ID and the numerical variable --------------------
+
+data<-read.csv(file.choose(),sep = ',')
+
+proportion<-tally(~data[,2],data=data,format='prop')[2]
+bootstrap_dist<-do(iterations)*rflip(nrow(data),proportion)
+
+par(mfrow=c(1,2))
+
+qqnorm(bootstrap_dist$prop); qqline(bootstrap_dist$prop, col = 2)
+hist(bootstrap_dist$prop,main='Histogram of the proportion')
+
+range<-quantile(sort(bootstrap_dist$prop),c(alpha/2,1-alpha/2))
+cat('The observed proportion is',round(proportion,6)*100,'%\n')
+cat('The confidence interval for the proportion is from',round(range[1],6)*100,'% to',round(range[2],6)*100,'%\n')
+cat('The margin of error is',round(proportion-range[1],6)*100,'%')
+
+dev.off()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
